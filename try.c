2d0ac08b101ed576795db933c206bfb860c69fdd
ahmed elsayed <elsayed815@gmail.com>
2022-01-21 16:52:51 +0200

    intial commit,creat repo

---
+++ try.c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+int main(){
+	char a[100];
+	char b[100];
+	printf("enter your first word ");
+	scanf("%s",a);
+	printf("enter your second word ");
+	scanf("%s",b);
+	while (a[i] == '\0' && b[i]=='c'){
+		
+		printf("same");
+	
+	}
+	else{
+		printf("not same");
+	}
+}
